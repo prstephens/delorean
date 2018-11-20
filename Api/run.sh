6cb8c2038974679896b0d6a8fdac4f55598b974d
Paul Stephens <pr.stephens@gmail.com>
2018-11-20 13:48:27 +0000

    updated the run command to stop exisiting container

--- Api/run.sh
+++ Api/run.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 # note: change the password value to the correct password for TM_PASS in production
+sudo docker stop $(sudo docker ps -q --filter ancestor=nodeapi)
+sudo docker image rm -f nodeapi
 sudo docker build -t nodeapi .
 sudo docker run -e NODE_ENV=production -e TM_PASS=password --net=host --restart=always -d nodeapi
 sudo docker system prune -f
