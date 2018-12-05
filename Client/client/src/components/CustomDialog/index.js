c8e3db389332d2b0b3e41d02940da721c954078d
Paul Stephens <pr.stephens@gmail.com>
2018-12-05 09:59:13 +0000

    Updated to latest Material Ui

--- Client/client/src/components/CustomDialog/index.js
+++ Client/client/src/components/CustomDialog/index.js
@@ -1,41 +1,48 @@
 import React from 'react'
-import FlatButton from 'material-ui/FlatButton'
-import Dialog from 'material-ui/Dialog'
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { closeDialog } from '../../actions'
 
-const CustomDialog = ({dialogTitle, dialogMessage, closeDialog }) => (
-      <Dialog
-        title={dialogTitle}
-        actions={[
-          <FlatButton
-            label="OK"
-            primary={true}
-            onClick={closeDialog}
-          />
-        ]}
-        modal={true}
-        open={dialogMessage != null}
-        onRequestClose={closeDialog}
-      >
+const CustomDialog = ({ dialogTitle, dialogMessage, closeDialog }) => (
+  <Dialog
+    open={dialogMessage != null}
+    onClose={closeDialog}
+    aria-labelledby="alert-dialog-title"
+    aria-describedby="alert-dialog-description"
+  >
+    <DialogTitle id="alert-dialog-title">{dialogTitle}</DialogTitle>
+    <DialogContent>
+      <DialogContentText id="alert-dialog-description">
         {dialogMessage}
-      </Dialog>
-  )
-  
-  CustomDialog.propTypes = {
-    dialogTitle: PropTypes.string,
-    dialogMessage: PropTypes.string,
-    closeDialog: PropTypes.func
-  }
-  
-  const mapStateToProps = (state) => ({
-    dialogTitle: state.delorean.dialogTitle,
-    dialogMessage: state.delorean.dialogMessage
-  })
-  
-  const mapDispatchToProps = {
-    closeDialog
-  };
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(CustomDialog)
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={closeDialog} color="primary" autoFocus>
+        OK
+    </Button>
+    </DialogActions>
+  </Dialog>
+)
+
+CustomDialog.propTypes = {
+  dialogTitle: PropTypes.string,
+  dialogMessage: PropTypes.string,
+  closeDialog: PropTypes.func
+}
+
+const mapStateToProps = (state) => ({
+  dialogTitle: state.delorean.dialogTitle,
+  dialogMessage: state.delorean.dialogMessage
+})
+
+const mapDispatchToProps = {
+  closeDialog
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CustomDialog)
